406b6a73620384c2d4758d537413064d6c7ce366
Aayush-WP <idkwhatocode@gmail.com>
2026-04-09 19:01:01 +0530

    Command sending capability added, other changes
    Can now use Serial0 to send commands for sensor data viewing; fixed a blocking bug in void loop() in ranging sensor part

--- unified_sensors_tx_rx/unified_sensors_tx_rx.ino
+++ unified_sensors_tx_rx/unified_sensors_tx_rx.ino
@@ -6,6 +6,11 @@
 volatile long encoderValue = 0;
 volatile long lastEncoded = 0;
 
+#define PWM_PIN_A 6
+#define DIR_PIN_A 7
+#define PWM_PIN_B 8
+#define DIR_PIN_B 9
+
 MS5837 barSensor;
 float fluidDensity = 997;
 
@@ -13,6 +18,7 @@ float fluidDensity = 997;
 uint8_t buf[16];
 int range_rx_index = 0;
 bool trigDone = false;
+bool readDone = false;
 long unsigned int T1_delay;
 int cmd_i;
 
@@ -59,9 +65,13 @@ long unsigned int rangingTP_rx = 330; //30Hz;
 IntervalTimer barTimer_tx;
 IntervalTimer rangingTimer_tx;
 IntervalTimer encoderTimer_tx;
-// IntervalTimer Pi_rx;
+IntervalTimer Pi_rx;
 
 int receivedCMD[10];   //value type and array length needs to be changed to fit the incoming cmd(dont know what it is currently)
+
+long unsigned int timestamp;
+long unsigned int time_period = 0;
+
 
 void setup()
 {
@@ -88,20 +98,24 @@ void setup()
   pinMode(EncoderPin2, INPUT);
   attachInterrupt(digitalPinToInterrupt(EncoderPin1), updateEncoder, CHANGE);
   attachInterrupt(digitalPinToInterrupt(EncoderPin2), updateEncoder, CHANGE);
-  
+
+  pinMode(PWM_PIN_A, OUTPUT);
+  pinMode(DIR_PIN_A, OUTPUT);
+  pinMode(PWM_PIN_B, OUTPUT);
+  pinMode(DIR_PIN_B, OUTPUT);
 
   //Interrupts Priority   !!change priority according to the rule => Highest for encoder receiving; then after that; higher priority for less frequently occuring events i.e. those with bigger time period
-  encoderTimer_tx.priority(1);
-  // Pi_rx.priority(2);
-  barTimer_tx.priority(2);
-  rangingTimer_tx.priority(3);
-  barTimer_tx.priority(4);
+  // encoderTimer_tx.priority(1);
+  // // Pi_rx.priority(2);
+  // barTimer_tx.priority(2);
+  // rangingTimer_tx.priority(3);
+  // barTimer_tx.priority(4);
 
   //Interrupts Begin
   barTimer_tx.begin(barSend, 50000); //20Hz
   rangingTimer_tx.begin(rangingSend, 66000);  //15Hz
-  encoderTimer_tx.begin(encoderSend, 14.2);   //70Khz
-  // Pi_rx.begin(piReceive, 50000);              //20Hz
+  encoderTimer_tx.begin(encoderSend, 70000);   //70Khz
+  Pi_rx.begin(piReceive, 50000);              //20Hz
 
 }
 
@@ -118,6 +132,11 @@ void setup()
 
 void loop()
 {
+  timestamp = micros();
+  // Serial.println(timestamp);
+  // Serial.println(millis());
+  // Serial.println(micros());
+  // Serial.println("HELLO");
   if ( millis() - barTimer >= barTP_rx )
   {
     barTimer = millis();
@@ -143,14 +162,19 @@ void loop()
   
   if ( trigDone && ((millis() - T1_delay) >= 10) )
   {
-    while ( range_rx_index < 16 )
+    if ( range_rx_index < 16 )      //replaced while with if
     {
       if ( Serial7.available() )
       {
         buf[range_rx_index++] = Serial7.read();
       }
     }
+    else
+      readDone = true;
+  }
 
+  if ( readDone )
+  {
     if ( buf[0] == 0xFF )
     {
       int calcsum = (buf[0] + buf[1] + buf[2]) & 0xFF;
@@ -158,35 +182,50 @@ void loop()
       {
         rangingPacket.distance = buf[1] * 256 + buf[2];
         trigDone = false;
+        readDone = false;
       }
       else
       {
         //CHECKSUM ERROR
         trigDone = false;
+        readDone = false;
       }
     }
     //HEADER NOT MATCHED
     trigDone = false;
+    readDone = false;
   }
 
-  if (Serial.available())
-  {
-    receivedCMD[cmd_i++] = Serial.read();
-    //perform actions using the received byte
-  }
-  else
-    cmd_i = 0;
+  // if (Serial.available())
+  // {
+  //   receivedCMD[cmd_i++] = Serial.read();
+  //   //perform actions using the received byte
+  // }
+  // else
+  //   cmd_i = 0;
+  if ( micros() - timestamp >= time_period )
+    time_period = micros() - timestamp;
+  Serial.println(time_period);
+  // Serial.println(millis() - timestamp);
+  // Serial.println(millis());
+  // Serial.println(micros());
 }
 
 void barSend ()
 {
   uint8_t buf[] = {barPacket.depth, barPacket.pressure, barPacket.temp};    //THIS WILL CAUSE PROBLEMS LATER, PLEASE FIX IT LATER ACCORDINGF TO RX DATA FROM RASPBERRY PI
-  Serial.write(buf, sizeof(buf));
+  // Serial.write(buf, sizeof(buf));
+  // Serial.println("bar packet");
+  // Serial.println(barPacket.temp);
+  // Serial.println();
 }
 
 void rangingSend ()
 {
-  Serial.write(rangingPacket.distance);
+  // Serial.write(rangingPacket.distance);
+  // Serial.println("ranging packet");
+  // Serial.println(rangingPacket.distance);
+  // Serial.println();
 }
 
 void encoderSend ()
@@ -194,16 +233,72 @@ void encoderSend ()
   noInterrupts();
   encoderPacket.encoder_copy = encoderValue;
   interrupts();
-  Serial.write(encoderPacket.encoder_copy);
+  // Serial.write(encoderPacket.encoder_copy);
+  // Serial.println("Encoder packet");
+  // Serial.print(encoderPacket.encoder_copy);
+  // Serial.println();
 }
 
-// void piReceive ()
-// {
-//   if (Serial.available)
-//   {
-//     incomingByte = Serial.read();
-//   }
-// }
+void piReceive ()
+{
+  if (Serial.available())
+  {
+    char incomingByte = Serial.read();
+    if (incomingByte == 'e')
+    {
+      Serial.println(encoderPacket.encoder_copy);
+    }
+    else if (incomingByte == 'b')
+    {
+      Serial.println(barPacket.depth);
+    }
+    else if (incomingByte == 'r')
+    {
+      Serial.println(rangingPacket.distance);
+    }
+    else if (incomingByte == 'm')
+    {
+      char motor = Serial.read();
+      int pwm = 0;
+      int byte;
+      // while ( (byte = Serial.read()) != ' ' )
+      for (int i = 0, base = 1; (byte = Serial.read()) != ' '; i++)
+      {
+        if ( i == 0)
+          base = 1;
+        else
+          base *= 10;
+        pwm += (byte - '0') * base;
+        // Serial.println(pwm);
+      }
+      int dir = Serial.read() - '0';
+      if ( motor == 'A' )
+      {
+        digitalWrite(DIR_PIN_A, dir);
+        analogWrite(PWM_PIN_A, pwm);
+        Serial.println(dir);
+        Serial.println(pwm);
+      }
+      else if (motor == 'B')
+      {
+        digitalWrite(DIR_PIN_B, dir);
+        analogWrite(PWM_PIN_B, pwm);
+        Serial.println(dir);
+        Serial.println(pwm);
+      }
+      else
+      {
+        Serial.print("Incorrect command: ");
+        Serial.println(motor);
+      }
+    }
+    else if (incomingByte != '\n' && incomingByte != '\r')
+    {
+      Serial.print("Incorrect command send: ");
+      Serial.println(incomingByte);
+    }
+  }
+}
 
 
 // //BAR SENSOR READ
